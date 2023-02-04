4fa3a5a0b7fa69af1d2abb9890d20e834aa2ace7
Farhad rasouli <rasouli.farhad@gmail.com>
2023-02-04 04:36:57 +0330

    add testUpperCaseAsFluxFunction test method for getting function as flux

--- deployer-example/src/test/java/com/farhad/example/function/deployer/ApplicationTest.java
+++ deployer-example/src/test/java/com/farhad/example/function/deployer/ApplicationTest.java
@@ -5,10 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import reactor.core.publisher.Flux;
+
 import org.springframework.context.ApplicationContext;
 
 import  org.springframework.cloud.function.context.FunctionCatalog;
 
+import java.util.List;
 import java.util.function.Function;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -58,5 +62,27 @@ public class ApplicationTest {
     }
    
 
+    @Test 
+    public void  testUpperCaseAsFluxFunction() {
+ 
+        String [] args = new String [] {
+                                 "--spring.cloud.function.location=/home/farhad/apps/my-examples/spring-cloud-function/deploying-packaged-functoion-example/normal-uppercase-function-example/target/normal-uppercase-function-example-0.0.1-SNAPSHOT.jar",
+                                 "--spring.cloud.function.function-class=com.farhad.example.function.uppercase.UppercaseFunction;com.farhad.example.function.uppercase.ReverseFunction"
+        };
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+            
+        FunctionCatalog functionCatalog = context.getBean(FunctionCatalog.class);
+        Function<Flux<String>,Flux<String>> uppercaseFunctionFlux = functionCatalog.lookup("uppercaseFunction");
+ 
+        Flux<String> fluxResult =  uppercaseFunctionFlux.apply(Flux.just("Hello","by"));
+
+        List<String> result =  fluxResult.collectList().block();
+
+        assertThat(result.get(0)).isEqualTo("HELLO");
+        assertThat(result.get(1)).isEqualTo("BY");
+ 
+     } 
+ 
+ 
 
 }
